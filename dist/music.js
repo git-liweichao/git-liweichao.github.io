46456bc2004a516e061a31576e666025fc0c91ae
git-liweichao <778903126@qq.com>
2021-02-16 23:40:31 +0800

    Site updated: 2021-02-16 23:40:31

---
+++ dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "夜空中最亮的星",
+        artist: '逃跑计划',
+        url: 'http://music.163.com/song/media/outer/url?id=25706282.mp3',
+        cover: 'http://p2.music.126.net/Eef2K2KV9dT3XUA6_Ve-Rw==/109951165543196748.jpg',
+      },
+      {
+        name: '知足',
+        artist: '五月天',
+        url: 'http://music.163.com/song/media/outer/url?id=385965.mp3',
+        cover: 'http://p3.music.126.net/_B1Fn_Z1WxHzqGLzLZDf-w==/109951163263882447.jpg',
+      },
+      {
+        name: '一千年以后',
+        artist: '林俊杰',
+        url: 'http://music.163.com/song/media/outer/url?id=26305531.mp3',
+        cover: 'http://p3.music.126.net/CcthX_ZCexIdriZADoNn3g==/109951165628166191.jpg',
+      },
+      {
+        name: '逍遥叹',
+        artist: '胡歌',
+        url: 'http://music.163.com/song/media/outer/url?id=4875306.mp3',
+        cover: 'http://p4.music.126.net/4tTN8CnR7wG4E1cauIPCvQ==/109951163240682406.jpg',
+      },
+      {
+        name: '如果当时',
+        artis: '许嵩',
+        url: 'http://music.163.com/song/media/outer/url?id=167870.mp3',
+        cover: 'http://p3.music.126.net/Md3RLH0fe2a_3dMDnfqoQg==/18590542604286213.jpg',
+      },
+      {
+        name: 'Someone Like You',
+        artis: 'Adele',
+        url: 'http://music.163.com/song/media/outer/url?id=16435049.mp3',
+        cover: 'http://p4.music.126.net/l-lffSJW45DaETAp7Vi22Q==/109951165341361103.jpg',
+      },
+      {
+        name: 'My Heart Will Go On',
+        artis: 'Celine Dion',
+        url: 'http://music.163.com/song/media/outer/url?id=2308499.mp3',
+        cover: 'http://p4.music.126.net/mO1OUNjZtCEIvAGdr7YXaA==/813638604607979.jpg',
+      }      
+    ]
+});
